40c19146a2b7336e2b58643b24d26a1ff90075b5
Kazimir <KazimirS46@gmail.com>
2022-12-25 14:17:57 +0400

    Create components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import './scss/app.scss';
+
+import { Header } from './components/Header';
+import { Categories } from './components/Categories';
+import { Sort } from './components/Sort';
+import { PizzaBlock } from './components/PizzaBlock';
+
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
